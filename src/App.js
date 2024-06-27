ae55b7bf76b6629aca1affe846884baf026942c1
waafi-adam <abdullah.waafi@gmail.com>
2024-06-27 23:31:56 +0800

    fix single pet page image

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ function App() {
           <Route
             path='checkout'
             element={
-                // <Checkout />
                 <PrivateRoute>
                   <Checkout />
                 </PrivateRoute>
--- src/components/PetImages.js
+++ src/components/PetImages.js
@@ -4,15 +4,15 @@ const PetImages = ({ images = [[]] }) => {
   const [main, setMain] = useState(images[0])
   return (
     <Wrapper>
-      <img src={main.url} alt='' className='main ' />
+      <img src={main} alt='' className='main ' />
       <div className='gallery'>
         {images.map((image, index) => {
           return (
             <img
-              src={image.url}
+              src={image}
               alt=''
               key={index}
-              className={`${image.url === main.url ? 'active' : null}`}
+              className={`${image === main ? 'active' : null}`}
               onClick={() => setMain(images[index])}
             />
           )
